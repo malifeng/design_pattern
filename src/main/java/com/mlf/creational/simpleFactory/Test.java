71a0cc6c42ca5c97976fc9dfe0bf1e0b7772a33f
mlf <lifeng.ma@unicareer.com>
2020-04-07 14:42:26 +0800

    add git.ignore

---
+++ src/main/java/com/mlf/creational/simpleFactory/Test.java
@@ -0,0 +1,9 @@
+package com.mlf.creational.simpleFactory;
+
+public class Test {
+    public static void main(String[] args) {
+        VideoFactory videoFactory = new VideoFactory();
+        Video video = videoFactory.getVideo("java");
+        video.produce();
+    }
+}
