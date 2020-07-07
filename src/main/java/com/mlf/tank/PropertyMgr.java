3f2b44a175a059cf6894a660de1973341276cd8e
mlf <lifeng.ma@unicareer.com>
2020-07-07 14:44:23 +0800

    添加配置文件

--- src/main/java/com/mlf/tank/PropertyMgr.java
+++ src/main/java/com/mlf/tank/PropertyMgr.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 import java.util.Properties;
 
 public class PropertyMgr {
-    static Properties props = null;
+    static Properties props = new Properties();
 
     static {
         try {
@@ -12,5 +12,15 @@ public class PropertyMgr {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    public static Object get(String key){
+        if(props == null ) return null;
+        return props.get(key);
+    }
+
+    public static void main(String[] args) {
+        Object initTankCount = PropertyMgr.get("initTankCount");
+        System.out.println(initTankCount);
     }
 }
--- src/main/java/com/mlf/tank/T.java
+++ src/main/java/com/mlf/tank/T.java
@@ -6,8 +6,9 @@ import java.util.Random;
 public class T {
     public static void main(String[] args) throws InterruptedException {
         TackFrame tackFrame = new TackFrame();
+        int initTankCount = Integer.parseInt((String) PropertyMgr.get("initTankCount"));
 
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < initTankCount; i++) {
             tackFrame.tanks.add(new Tank(50 + i* 30,200,Dir.DOWN,tackFrame,Group.BLACK));
         }
 
